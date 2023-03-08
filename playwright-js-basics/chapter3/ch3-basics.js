6bbf28f917b57938b21e2cb0c8632bf9d30092c3
Michal Zak <zako05@gmail.com>
2023-03-08 10:58:15 +0100

    add chapter3 basics (type & click)

---
+++ playwright-js-basics/chapter3/ch3-basics.js
@@ -0,0 +1,13 @@
+const { chromium } = require('playwright');
+
+(async() => {
+  const browser = await chromium.launch({ headless: false, slowMo: 100})
+  const page = await browser.newPage()
+  await page.goto('https://the-internet.herokuapp.com/forgot_password')
+  const email = await page.$('#email')
+  // .$ is deprecate, use page.locator() instead
+  // const email = page.locator('#email')
+  await email.type('zako05@gmail.com', { delay: 50 })
+  await page.click('button#form_submit')
+  await browser.close()
+})();
